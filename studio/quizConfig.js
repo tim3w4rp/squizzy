5ec875c3f239fbd2daa6a65f08965c98ae0c9def
Martin Jordán <martin@jordan.cz>
2023-02-17 03:07:31 +0100

    Change remoteWebHost

--- studio/quizConfig.js
+++ studio/quizConfig.js
@@ -1,7 +1,7 @@
 export default {
   localWebHost: 'http://localhost:3000', // default Vue port
   localApiHost: 'http://localhost:3000', // default Now API port
-  remoteWebHost: '<your-app-name>.now.sh',
+  remoteWebHost: 'https://squizzy.martinjordan.dev',
   match: {
     correctAnswerScore: 100,
     firstAnswerScore: 50
